0bc916ecb97cfee2c5c138bce8006f2430945f21
unknown <andres.quintero98@hotmail.com>
2020-02-20 12:35:44 -0500

    Genesis

---
+++ find-eci/src/main/java/edu/eci/arsw/model/Intereses.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.model;
+
+import java.util.ArrayList;
+
+public class Intereses {
+
+    private Genero sexoInteres;
+    private TipoRelacion tipoR;
+    private ArrayList<String> aspectosImportantes;
+}
---
+++ find-eci/src/main/java/edu/eci/arsw/model/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.model;
+
+/**
+ *
+ * @author andres.quintero-d
+ */
+public class Usuario {
+
+    private String Correo;
+    private String Nombre;
+    private int Edad;
+    private Genero sexo;
+    private int altura;
+    private int celular;
+    private String cuentaIG;
+}
